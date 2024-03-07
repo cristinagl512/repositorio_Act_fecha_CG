ed49615f38b4fdc078faea2991910b7084cc1ef7
Cristina González <cris.glez512@gmail.com>
2024-03-07 10:02:47 +0100

    versión Función

--- src/main/kotlin/Fecha.kt
+++ src/main/kotlin/Fecha.kt
@@ -9,12 +9,17 @@ class Fecha(private val d: Int, private val m: Int, private val a: Int) {
         if (m < 1 || m > 12) return false
 
         // determinamos la cantidad de días del mes:
+        var diasMes = i()
+        return if (d > diasMes) false else true
+    } // … más métodos
+
+    private fun i(): Int {
         var diasMes = 0
         when (m) {
             1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
             4, 6, 9, 11 -> diasMes = 30
             2 -> diasMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
         }
-        return if (d > diasMes) false else true
-    } // … más métodos
+        return diasMes
+    }
 }
