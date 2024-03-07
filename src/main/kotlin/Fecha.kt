a256ef9fce3f2c638c9c16d0e6d3ace2486a1624
Cristina González <cris.glez512@gmail.com>
2024-03-07 10:07:25 +0100

    Cambios variables

--- src/main/kotlin/Fecha.kt
+++ src/main/kotlin/Fecha.kt
@@ -1,25 +1,20 @@
 package org.example
 
-class Fecha(private val d: Int, private val m: Int, private val a: Int) {
+class Fecha(private val dias: Int, private val mes: Int, private val a: Int) {
 
 
     fun valida(): Boolean {
 
-        if (d < 1 || d > 31) return false
-        if (m < 1 || m > 12) return false
+        if (dias < 1 || dias > 31) return false
+        if (mes < 1 || mes > 12) return false
 
         // determinamos la cantidad de días del mes:
-        var diasMes = i()
-        return if (d > diasMes) false else true
-    } // … más métodos
-
-    private fun i(): Int {
-        var diasMes = 0
-        when (m) {
-            1, 3, 5, 7, 8, 10, 12 -> diasMes = 31
-            4, 6, 9, 11 -> diasMes = 30
-            2 -> diasMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
+        var diasdelMes = 0
+        when (mes) {
+            1, 3, 5, 7, 8, 10, 12 -> diasdelMes = 31
+            4, 6, 9, 11 -> diasdelMes = 30
+            2 -> diasdelMes = if (a % 400 == 0 || a % 4 == 0 && a % 100 != 0) 29 else 28
         }
-        return diasMes
-    }
+        return if (dias > diasdelMes) false else true
+    } // … más métodos
 }
